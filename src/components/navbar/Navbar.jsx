1d56a7d964a94367d8cbe7e0bc5907b1f936f54e
FayeLV <faye.l.tech@gmail.com>
2024-07-17 02:34:35 -0500

    Fixedd routes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,48 +1,5 @@
-// import React, { useState } from "react";
-// import "./Navbar.css";
-// import Logo from "../../assets/nav/menuIcon.png";
-// import Close from "../../assets/nav/closeIcon.png";
-
-// const Navbar = () => {
-//   const [menuOpen, setMenuOpen] = useState(false);
-
-//   return (
-//     <nav className="navbar">
-//       <a className="title" href="/">
-//         Faye.div
-//       </a>
-//       <div className="menu">
-//         <ul className={`menuItems ${menuOpen ? "menuOpen" : ""}`}>
-//           <li>
-//             <a href="/">Home</a>
-//           </li>
-//           <li>
-//             <a href="#about">About</a>
-//           </li>
-//           <li>
-//             <a href="#projects">Projects</a>
-//           </li>
-//           <li>
-//             <a href="#contact">Contact</a>
-//           </li>
-//         </ul>
-//       </div>
-
-//       <div className="menuToggle" onClick={() => setMenuOpen(!menuOpen)}>
-//         <span></span>
-//         <span></span>
-//         <span></span>
-//       </div>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import "./Navbar.css";
-import Logo from "../../assets/nav/menuIcon.png";
-import Close from "../../assets/nav/closeIcon.png";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -53,14 +10,13 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <a className="title" href="/">
-        Faye.div
-      </a>
+      <h1 className="title">Faye.div</h1>
+
       <div className="menu">
         <ul className={`menuItems ${menuOpen ? "menuOpen" : ""}`}>
           <li>
-            <a href="/" onClick={handleMenuItemClick}>
-              Home
+            <a href="/my-portfolio" onClick={handleMenuItemClick}>
+              About
             </a>
           </li>
           <li>
